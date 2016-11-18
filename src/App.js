8ba2838d6173f90e7d0a62b0bf2c67211693fc30
Robin Wieruch <wrobin@gmx.net>
2016-11-18 18:06:18 +0100

    style

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ class App extends Component {
     const { results, resultKey, query, isLoading } = this.state;
 
     return (
-      <div>
-        <div>
+      <div className="page">
+        <div className="search">
           <InputConfirm query={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
             Search
           </InputConfirm>
@@ -77,24 +77,26 @@ class App extends Component {
 }
 
 const HitsTable = ({ results, resultKey, isLoading }) =>
-  <div>
-    <div>
-      <span>Title</span>
-      <span>Author</span>
-      <span>Comment Count</span>
-      <span>Points</span>
+  <div className="table">
+    <div className="table-header">
+      <span style={{ width: '40%' }}>Title</span>
+      <span style={{ width: '30%' }}>Author</span>
+      <span style={{ width: '15%' }}>Comments</span>
+      <span style={{ width: '15%' }}>Points</span>
     </div>
-    { isLoading ?
-        <div>Loading ...</div> :
-        results[resultKey].hits.map((item, key) =>
-          <div key={key}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )
-    }
+    <div>
+      { isLoading ?
+          <div className="table-empty">Loading ...</div> :
+          results[resultKey].hits.map((item, key) =>
+            <div className="table-row" key={key}>
+              <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
+              <span style={{ width: '30%' }}>{item.author}</span>
+              <span style={{ width: '15%' }}>{item.num_comments}</span>
+              <span style={{ width: '15%' }}>{item.points}</span>
+            </div>
+          )
+      }
+    </div>
   </div>
 
 const InputConfirm = ({ query, onChange, onSubmit, children }) =>
